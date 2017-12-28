13ae41f7c58ead40fc8780600205b53d763c38ee
GitMeGet <tanyupeng1994@gmail.com>
2017-12-28 08:21:44 -0800

    Initial commit

---
+++ app/src/main/java/com/example/yupeng/boombox/MusicController.java
@@ -0,0 +1,18 @@
+package com.example.yupeng.boombox;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by Yu Peng on 21/11/2017.
+ */
+
+public class MusicController extends MediaController {
+
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){};
+
+}
---
+++ app/src/main/java/com/example/yupeng/boombox/Song.java
@@ -0,0 +1,24 @@
+package com.example.yupeng.boombox;
+
+/**
+ * Created by Yu Peng on 7/11/2017.
+ */
+
+public class Song {
+
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist) {
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+
+
+}
